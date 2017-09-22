94b9ba89e0aa84b6846f81ccdb3ece0c88081178
GuoChang <827789895@qq.com>
2017-09-22 14:21:50 +0800

    get_product_desc_keys

--- us_data_parser.py
+++ us_data_parser.py
@@ -5,3 +5,47 @@
 # @Site    : https://github.com/xiphodon
 # @File    : us_data_parser.py
 # @Software: PyCharm
+
+import pandas as pd
+import os
+import numpy as np
+import util
+
+base_path = r"E:\work_all\topease\US_data\database_sep_tab\dbo_usa_201607_09"
+
+def read_us_data():
+    '''
+    读取美国海关数据（2016.07 - 2016.09）
+    :return:
+    '''
+    file_list = os.listdir(base_path)
+
+    col_name = ['HS Code', 'Actual Arrival Date', 'Consignee Name', 'Shipper Name', 'Product Desc', 'Country']
+    data = pd.DataFrame(columns=col_name)
+
+    for file_name in file_list:
+        file_path = os.path.join(base_path,file_name)
+        temp_data = pd.read_csv(file_path, encoding='ansi', sep='\t')
+
+        temp_data = temp_data[col_name]
+        temp_data['Country'] = list(map(lambda x: str(x).split(',')[-1].strip(), temp_data['Country']))
+
+        # data = pd.concat([data,temp_data],axis=0)
+        data = data.append(temp_data)
+
+        print(file_path)
+        print(len(data))
+
+        return data
+
+    return data
+
+
+
+if __name__ == '__main__':
+    data = read_us_data()
+    product_desc_list = data['Product Desc']
+    product_desc_map = map(lambda x:str(x).replace('<br/>', ''), product_desc_list)
+    data['product_desc_keys'] = list(map(util.get_key_words, product_desc_map))
+    # 存数据
+    print(data['product_desc_keys'])
--- util.py
+++ util.py
@@ -5,3 +5,27 @@
 # @Site    : https://github.com/xiphodon
 # @File    : util.py
 # @Software: PyCharm
+
+
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize,word_tokenize
+
+
+def get_key_words(content):
+    '''
+    获取内容中关键字
+    :param content:
+    :return: 关键字列表
+    '''
+    content = str(content)
+    # 提取关键字(小写)
+    word_list = word_tokenize(content.lower())
+    # 分析关键字词性
+    word_pos_tag_set = set(nltk.pos_tag(word_list))
+    # 筛选名词类关键词
+    word_pos_tag_NN = [item[0] for item in word_pos_tag_set if 'NN' in item[1]]
+
+    return word_pos_tag_NN
