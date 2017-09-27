8cf76fee1c23e4368cbacdd2049a1f76bab4ebb6
GuoChang <827789895@qq.com>
2017-09-27 16:43:18 +0800

    解决部分无法翻译中文的bug

--- cn_data_parser.py
+++ cn_data_parser.py
@@ -19,7 +19,12 @@ import urllib.parse
 
 base_path = r"E:\work_all\topease\CN_data\database_sep_^\201604_201611"
 
-debug = True
+debug = False
+# 翻译缓存开关
+cache_switch = True
+
+other_1 = True
+other_2 = False
 
 
 def read_cn_data():
@@ -65,17 +70,28 @@ headers = {'Host': 'translate.google.cn',
            'Connection': 'keep-alive'}
 
 
-# 翻译缓存开关
-cache_switch = True
-# 缓存的翻译词组对（翻译对照表）
-cache_cn2en_dict = {}
+
+def read_cache_cn2en_dict():
+    '''
+    读取翻译对照表
+    :return:
+    '''
+    if os.path.exists(cache_cn2en_path):
+        with open(cache_cn2en_path, 'r') as fp:
+            cache_cn2en_stream = fp.read()
+            return json.loads(cache_cn2en_stream)
+    else:
+        return {}
+
+
+
+
+### 缓存的翻译词组对（翻译对照表）
 # 对照表地址
 cache_cn2en_path = r'./temp_data/cache_cn2en_dict.json'
 # 读取翻译对照表
-if os.path.exists(cache_cn2en_path):
-    with open(cache_cn2en_path,'r') as fp:
-        cache_cn2en_stream = fp.read()
-        cache_cn2en_dict = json.loads(cache_cn2en_stream)
+cache_cn2en_dict = read_cache_cn2en_dict()
+
 
 
 def cache_cn2en(cn_str):
@@ -86,7 +102,7 @@ def cache_cn2en(cn_str):
     '''
     if cache_switch:
         # 进行缓存
-        if cn_str not in cache_cn2en_dict:
+        if cn_str not in cache_cn2en_dict or 'en_desc' not in cache_cn2en_dict[cn_str]:
             cache_cn2en_cnstr_dict = cache_cn2en_dict[cn_str] = {}
             cache_cn2en_cnstr_dict['used_times'] = 0
             cache_cn2en_cnstr_dict['en_desc'] = cn2en(cn_str)
@@ -107,11 +123,20 @@ def cn2en(cn_str):
     :return:
     '''
 
-    encode_cn_str = urllib.parse.quote(cn_str)
-    r = requests.get(google_translate_url + encode_cn_str, headers=headers)
-    en_str = json.loads(r.text)[0][0][0]
+    # time.sleep(0.5)
 
-    return en_str
+    try:
+        encode_cn_str = urllib.parse.quote(cn_str)
+        r = requests.get(google_translate_url + encode_cn_str, headers=headers)
+        en_str = json.loads(r.text)[0][0][0]
+
+        if len(en_str) > 0:
+            return en_str
+        else:
+            return ''
+    except Exception as e:
+        print(cn_str,e)
+        return ''
 
 
 def save_cache_cn2en_dict():
@@ -123,27 +148,46 @@ def save_cache_cn2en_dict():
         fp.write(json.dumps(cache_cn2en_dict))
 
 
+def save_new_cn_data():
+    '''
+    持久化中国海关数据
+    :return:
+    '''
+    if debug:
+        data.to_csv(r'./temp_data/cn_10000.csv', sep='\t', index=False, encoding='utf8')
+    else:
+        data.to_csv(r'./temp_data/cn_2016_04_2016_11.csv', sep='\t', index=False, encoding='utf8')
+
+
 if __name__ == '__main__':
-    print(datetime.datetime.now())
-    time1 = time.time()
+    try:
+        print(datetime.datetime.now())
+        time1 = time.time()
 
-    data = read_cn_data()
-    print("====== get data OK ======")
+        data = read_cn_data()
+        print("====== get data OK ======")
 
-    data['hs2desc'] = [util.hs2desc(item) for item in data['海关编码']]
-    print("====== get hs2desc OK ======")
+        data['hs2desc'] = [util.hs2desc(item) for item in data['海关编码']]
+        print("====== get hs2desc OK ======")
 
-    data['cn_product_desc2en_product_desc'] = [cache_cn2en(item) for item in data['商品名称']]
-    print("====== cn_product_desc2en_product_desc ======")
+        data['cn_product_desc2en_product_desc'] = [cache_cn2en(item) for item in data['商品名称']]
+        print("====== cn_product_desc2en_product_desc ======")
 
-    data.to_csv(r'./temp_data/cn_10000.csv', sep='\t', index=False, encoding='utf8')
-    print("====== save data OK ======")
+        save_new_cn_data()
+        print("====== save data OK ======")
 
-    save_cache_cn2en_dict()
-    print("====== save save_cache_cn2en_dict OK ======")
+        save_cache_cn2en_dict()
+        print("====== save save_cache_cn2en_dict OK ======")
 
-    print("finish")
+        print("finish")
 
-    print(datetime.datetime.now())
-    time2 = time.time()
-    print((time2 - time1))
+        print(datetime.datetime.now())
+        time2 = time.time()
+        print((time2 - time1))
+
+    except Exception as e:
+        saved_cache_cn2en_dict = read_cache_cn2en_dict()
+        if len(cache_cn2en_dict) > len(saved_cache_cn2en_dict):
+            save_cache_cn2en_dict()
+        print("finish(Exception)")
+        raise e
