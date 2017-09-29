dd7ad2eadb5b688f3418486d696c11482a522348
GuoChang <827789895@qq.com>
2017-09-29 15:22:17 +0800

    新建中美数据联合查询任务

---
+++ cn_us_unite_query.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/29 15:21
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : cn_us_unite_query.py
+# @Software: PyCharm
